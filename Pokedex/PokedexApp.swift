eb582e51700682f8badc88d24152f83c9b475ccd
Adrian Castañeda Rodriguez <ing.adrian.castaneda@gmail.com>
2026-07-01 14:02:25 -0700

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Adrian Castañeda on 01/07/26.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
